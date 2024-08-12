bf5eb6302294ee2609f4119d301a5a58fda1bca1
SWAGer <131869327+Puker228@users.noreply.github.com>
2024-08-12 18:06:53 +0300

    todo model

---
+++ src/models/todo.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Date
+from datetime import date
+
+from src.database import Base
+
+
+class ToDo(Base):
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str | None]
+    end_date: Mapped[date | None] = mapped_column(Date)
+    important: Mapped[bool] = mapped_column(default=False)
