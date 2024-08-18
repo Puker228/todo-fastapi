a4ffbab82840841f264b9040fb71af018baccb2e
SWAGer <131869327+Puker228@users.noreply.github.com>
2024-08-18 21:02:33 +0300

    new db

--- app/config.py
+++ app/config.py
@@ -1,22 +1,18 @@
-from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel
-
-
-BASE_DIR = Path(__file__).parent.parent
-
-
-class AuthJWT(BaseModel):
-    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
-    public_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
-    algorithm: str = "RS256"
-    access_token_expire_minutes: int = 3
 
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///./app/db/todo.db"
-    db_echo: bool = False
-    auth_jwt: AuthJWT = AuthJWT()
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 settings = Settings()
